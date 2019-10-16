97df380bfbd7d374c0c7007d8119a4ae3dbe4578
adrian <r3v@protonmail.ch>
2019-10-16 21:59:22 +0200

    # Change shopware auth  ** change shopware auth  ** add shopware auth docuemntation

--- app/Http/Middleware/ShopwareAuth.php
+++ app/Http/Middleware/ShopwareAuth.php
@@ -6,14 +6,17 @@ namespace App\Http\Middleware;
 
 use App\AuthProviders\ShopwareProvider;
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class ShopwareAuth
 {
 
-    public function handle($request, Closure $next) {
-        $authService = new ShopwareProvider($request->get('url'), $request->get('username'), $request->get('api_key'));
-        $result = $authService->get($request->get('user_id'));
+    public function handle(Request $request, Closure $next) {
+        $header = $request->headers->get('X-AUTH');
+        $user_data = json_decode(base64_decode($header), true);
+        $authService = new ShopwareProvider($user_data['url'], $user_data['username'], $user_data['api_key']);
+        $result = $authService->get($user_data['user_id']);
         if($result->status() !== Response::HTTP_OK){
             return response()->json(['content' => [], 'error_messages' => []], Response::HTTP_UNAUTHORIZED);
         }
